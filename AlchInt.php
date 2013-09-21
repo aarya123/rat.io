914f17da0c5710ea45b29e357897541d29996f0f
Anubhaw Arya <anubhaw.arya@gmail.com>
2013-09-21 02:14:42 -0400

    Moved api key around

--- AlchInt.php
+++ AlchInt.php
@@ -1,6 +1,5 @@
 <?php
 include "/module/AlchemyAPI.php";
-$ALCHEMYAPIKEY="f3320e80d4cb912bc1e789c94b07a4471d992ddf";
 $AlchemyObj = new AlchemyAPI();
-$AlchemyObj->setAPIKey($ALCHEMYAPIKEY);
+$AlchemyObj->setAPIKey("f3320e80d4cb912bc1e789c94b07a4471d992ddf");
 ?>
