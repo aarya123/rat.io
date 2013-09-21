46c4438bf898a5a5432c5e73ce62ac5741c1458d
Anubhaw Arya <anubhaw.arya@gmail.com>
2013-09-21 21:43:58 +0000

    Fixed GoogleReviews

--- GoogleReviews.php
+++ GoogleReviews.php
@@ -1,24 +1,38 @@
 <?php
 function getGoogleReviewScore($query) {
     include "AlchInt.php";
-    $result=json_decode(file_get_contents("https://maps.googleapis.com/maps/api/place/textsearch/json?query=".urlencode($query)."&sensor=false&key=AIzaSyAw3G2Nm0A7gDa8dzKm3UnBI6CPxIHkayo"),true)["results"];
+    $result=json_decode(file_get_contents("https://maps.googleapis.com/maps/api/place/textsearch/json?query=".urlencode($query)."&sensor=false&key=AIzaSyDs4lsfh1xT0F6xL_liNt6pPtvsp4rSd8g"),true)["results"];
     $IDs=array();
-    for($x=0;$x<sizeof($result);$x++)
-        $IDs[$x]=$result[$x]["reference"];
+    if(sizeof($result)<5)
+        for($x=0;$x<sizeof($result);$x++)
+            $IDs[$x]=$result[$x]["reference"];
+    else
+        for($x=0;$x<5;$x++)
+            $IDs[$x]=$result[$x]["reference"];
     $score=0;
     $count=0;
     for($x=0;$x<sizeof($IDs);$x++)
     {
-        $reviews=json_decode(file_get_contents("https://maps.googleapis.com/maps/api/place/details/json?reference=".$IDs[$x]."&sensor=true&key=AIzaSyAw3G2Nm0A7gDa8dzKm3UnBI6CPxIHkayo"),true)["result"]["reviews"];
+        $reviews=json_decode(file_get_contents("https://maps.googleapis.com/maps/api/place/details/json?reference=".$IDs[$x]."&sensor=true&key=AIzaSyDs4lsfh1xT0F6xL_liNt6pPtvsp4rSd8g"),true)["result"];
+        if(isset($reviews["reviews"]))
+            $reviews=$reviews["reviews"];
+        else
+            continue;
         for($y=0;$y<sizeof($reviews);$y++)
         {
-            try{
+            try
+            {
                 $outputAlch = $AlchemyObj->TextGetTextSentiment($reviews[$y]["text"], AlchemyAPI::JSON_OUTPUT_MODE);
                 $output = json_decode($outputAlch, true);
-                $score += $output['docSentiment']['score'];
-                $count++;
+                if(isset($output['docSentiment']['score']))
+                {
+                    $score += $output['docSentiment']['score'];
+                    $count++;
+                }
             }
-            catch(Exception $e){}
+            catch(Exception $e){
+                //Weird errors...
+            }
         }
     }
     if($count!=0)
