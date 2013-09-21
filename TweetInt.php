82755cb86a9e919e46778924b429ac20fc18a865
green131 <singkiwi@hotmail.com>
2013-09-21 14:29:47 -0400

    Updated TweetInt.php

--- TweetInt.php
+++ TweetInt.php
@@ -1,2 +1,12 @@
 <?php
 
+include 'AlchInt.php';
+
+try {
+    $keywords = $_GET["keywords"];
+    $score = $AlchemyObj->URLGetTextSentiment("https://twitter.com/search?q=%23".$keywords, AlchemyAPI::JSON_OUTPUT_MODE);
+    echo $score
+} catch(Exception $e) {
+  echo $e->getMessage();
+}
+    
