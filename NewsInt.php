f53f5c985abd977f0ef4450367030b5c63f55306
mgottein <mgottein@purdue.edu>
2013-09-22 02:39:19 +0000

    Fixed some problems with -news

--- NewsInt.php
+++ NewsInt.php
@@ -1,13 +1,40 @@
 <?php
 function getNewsScore($query) {
-	try {
-		include 'AlchInt.php';
-	    $outputAlch = $AlchemyObj->URLGetTextSentiment("https://news.google.com/news/feeds?q=".$query, AlchemyAPI::JSON_OUTPUT_MODE);
-	    $output = json_decode($outputAlch, true);
-	    $score = $output['docSentiment']['score'];
-	    return $score * 5;
-	} catch(Exception $e) {
-    	return 2;
-    }
+	include 'AlchInt.php';
+	$query = urlencode($query);
+	$searchResults = json_decode(file_get_contents("https://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=$query"), true);
+	if($searchResults != null && array_key_exists("responseData", $searchResults)) {
+	    $searchResults = $searchResults["responseData"];
+	    if($searchResults != null && array_key_exists("results", $searchResults)) {
+	        $searchResults = $searchResults["results"];
+	    }
+	    else {
+	        return 2;
+	    }
+	}
+	else {
+	    return 2;
+	}
+	$sentiments = array();
+	foreach($searchResults as $result) {
+	    try {
+	        array_push($sentiments, json_decode($AlchemyObj->URLGetTextSentiment($result["unescapedUrl"], AlchemyAPI::JSON_OUTPUT_MODE), true)['docSentiment']['score']);
+	    }
+	    catch(Exception $e) {
+	    }
+	}
+	$size = sizeof($sentiments);
+	if($size > 0) {
+    	sort($sentiments);
+    	if($size % 2 == 0) {
+    	    return $sentiments[$size / 2];
+    	}
+    	else {
+    	    return ($sentiments[$size / 2] + $sentiments[($size / 2) + 1]) / 2;
+    	}
+	}
+	else {
+	    return 2;
+	}
 }
 ?>
